d828732197d28dd235ec1bc27b6f6ab985567a3f
Stefan Dobrev <stefandobrev88@gmail.com>
2023-01-05 21:06:31 +0200

    message

---
+++ index.js
@@ -0,0 +1,15 @@
+// console.log("Hello world"); 
+const firstName = "Stefan";
+let lastName = "Dobrev";
+var middleName = "T";
+
+console.log(`${firstName} ${middleName} ${lastName}`);
+console.log(firstName + " " + middleName + " " + lastName);
+
+const text = "Nqkakav text";
+const number = 34534.4;
+const number1 = 34534;
+
+const proverka = true; // false
+
+const users = ["Manol", "Petar", "Joni"];
